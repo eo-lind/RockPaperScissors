875b6c0de86568888acf566938d252e26f3da549
Olivia Lind <eo.lind@gmail.com>
2022-05-29 00:23:50 -0500

    didn't save program.cs before last push

--- Program.cs
+++ Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace RockPaperScissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ------------------------------------------------------------- //
+            // TODO: execute program here
+            Console.WriteLine("What will it be? Rock, Paper, or Scissors?");
+
+            string playerChoice = Console.ReadLine().ToLower();
+
+            if (playerChoice == "rock")
+            {
+                Console.WriteLine($@"    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)");
+            }
+            else if (playerChoice == "paper")
+            {
+                Console.WriteLine($@"     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)");
+            }
+            else if (playerChoice == "scissors")
+            {
+                Console.WriteLine($@"    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)");
+            }
+            else {
+                Console.WriteLine("Sorry, that was not a valid choice. Goodbye.");
+            }
+
+            // ------------------------------------------------------------- //
+        }
+    }
+}
